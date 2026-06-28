2fcf7dbd250a2866259a44f7a94cacec7aa7403b
Brandon Barrera <brger001@gmail.com>
2026-06-28 01:05:54 -0500

    se agrego columnas a la tabla partidos para su trazabilidad

--- sql/03_ddl_partidos.sql
+++ sql/03_ddl_partidos.sql
@@ -2,8 +2,11 @@ IF NOT EXISTS (SELECT * FROM sys.tables WHERE name = 'partidos' AND schema_id = 
 BEGIN
 CREATE TABLE bronze.partidos (
     id INT PRIMARY KEY,
-    utcDate NVARCHAR(50),
-    lastUpdated NVARCHAR(50),
+    utcdate NVARCHAR(50),
+    matchday INT,
+    home_team_id INT,
+    away_team_id INT,
+    lastupdated NVARCHAR(50),
     [status] NVARCHAR(20),
     liga_archivo NVARCHAR(10),
     temporada_archivo NVARCHAR(10),
